425ae5500e6a80ed75be902b474432f151152b9e
Keshav <keshavsoni098@gmail.com>
2021-11-11 18:27:09 +0530

    AC

--- Others/aggressive_cows.cpp
+++ Others/aggressive_cows.cpp
@@ -19,8 +19,46 @@ void trace(ll a[],ll n){for(ll i=0;i<n;i++)cout<<a[i]<<" ";	cout<<"\n";}
 void trace(vector<ll> v){for(auto x:v)cout<<x<<" "; cout<<"\n";}
 void trace(map<ll,ll> m){for(auto x:m)cout<<x.first<<" -> "<<x.second<<"\n";}
 
-void solve(){
+int n,c;
+int x[100000];
 
+// gets converted to 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 
+/// problem.. Then just apply binary search to find the
+// last valid d i.e the last 1 after which 0's start
+
+bool check(int d){       // d is the minimum distance
+	int last=-1e9; 		// last stall in which cow was placed
+	int cnt=0;			 // max number of cows accomodated
+	for(int i=0;i<n;i++){ 
+		if(x[i]-last>=d){
+			cnt++; 		//we put the cow in the ith stall
+			last=x[i];
+		}
+	}
+	if(cnt>=c) 		// we can put atleast c cows 
+		return 1;
+	else
+		return 0;
+}
+
+void solve(){
+	cin>>n>>c;
+	for(int i=0;i<n;i++)	
+		cin>>x[i];
+	sort(x,x+n);
+	
+	int l=1,h=1e9;
+	int ans;
+	while(l<=h){
+		int mid=(l+h)/2;
+		if(check(mid)){
+			ans=mid;
+			l=mid+1;
+		}
+		else
+			h=mid-1;
+	}
+	cout<<ans<<"\n";		
 }
 
 signed main()
